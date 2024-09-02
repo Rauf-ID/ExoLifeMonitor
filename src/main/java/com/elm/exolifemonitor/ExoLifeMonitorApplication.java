30a6630c4b0e721923fa3619307375d57c697a64
Rauf-ID <rauf.develop@gmail.com>
2024-09-02 21:14:32 +0500

    Added Spring Boot application template

---
+++ src/main/java/com/elm/exolifemonitor/ExoLifeMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.elm.exolifemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExoLifeMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExoLifeMonitorApplication.class, args);
+	}
+
+}
