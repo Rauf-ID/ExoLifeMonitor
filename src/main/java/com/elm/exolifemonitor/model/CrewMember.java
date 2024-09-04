b371de6d86c07e40e325b2d0d7313f9c88c5fffa
Rauf-ID <rauf.develop@gmail.com>
2024-09-04 19:54:26 +0500

    Added Lombok library, added entities and configuration for connecting to the database

---
+++ src/main/java/com/elm/exolifemonitor/model/CrewMember.java
@@ -0,0 +1,52 @@
+/*
+ * This file is part of ExoLifeMonitor-Java.
+ *
+ * ExoLifeMonitor-Java is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ExoLifeMonitor-Java is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ExoLifeMonitor-Java.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2024 Rauf Agaguliev
+ */
+
+package com.elm.exolifemonitor.model;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.SequenceGenerator;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "crew_members")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CrewMember {
+
+    @Id
+    @SequenceGenerator(name = "crew_members_seq", sequenceName = "crew_members_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "crew_members_seq")
+    private Long id;
+    private String name;
+    private String role;
+    @ManyToOne
+    @JoinColumn(name = "station_id")
+    private Station station;
+
+}
+
---
+++ src/main/java/com/elm/exolifemonitor/model/Resources.java
@@ -0,0 +1,53 @@
+/*
+ * This file is part of ExoLifeMonitor-Java.
+ *
+ * ExoLifeMonitor-Java is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ExoLifeMonitor-Java is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ExoLifeMonitor-Java.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2024 Rauf Agaguliev
+ */
+
+package com.elm.exolifemonitor.model;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.SequenceGenerator;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "resources")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Resources {
+
+    @Id
+    @SequenceGenerator(name = "resources_seq", sequenceName = "resources_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "resources_seq")
+    private Long id;
+    private String type;
+    private float currentLevel;
+    private float capacity;
+    @ManyToOne
+    @JoinColumn(name = "station_id")
+    private Station station;
+
+}
+
