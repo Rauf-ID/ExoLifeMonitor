ffee6846cfc780695e8dcd4b4eea4d4012717591
Rauf-ID <rauf.develop@gmail.com>
2024-09-05 20:57:08 +0500

    Added JPA classes for working with the database

---
+++ src/main/java/com/elm/exolifemonitor/repository/HealthDataRepository.java
@@ -0,0 +1,33 @@
+/*
+ * This file is part of ExoLifeMonitor-Java.
+ *
+ * ExoLifeMonitor-Java is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ExoLifeMonitor-Java is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ExoLifeMonitor-Java.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2024 Rauf Agaguliev
+ */
+
+package com.elm.exolifemonitor.repository;
+
+import com.elm.exolifemonitor.model.HealthData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HealthDataRepository extends JpaRepository<HealthData, Long> {
+
+    List<HealthData> findByCrewMemberId(Long crewMemberId);
+
+}
