5053868bd4c3861422d5b0172f43648ebca8bb4f
Rauf-ID <rauf.develop@gmail.com>
2024-09-07 22:01:02 +0500

    Added Kafka consumer classes

---
+++ src/main/java/com/elm/exolifemonitor/kafka/IncidentKafkaConsumer.java
@@ -0,0 +1,32 @@
+/*
+ * This file is part of ExoLifeMonitor-Java.
+ *
+ * ExoLifeMonitor-Java is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ExoLifeMonitor-Java is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ExoLifeMonitor-Java.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2024 Rauf Agaguliev
+ */
+
+package com.elm.exolifemonitor.kafka;
+
+import com.elm.exolifemonitor.service.IncidentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IncidentKafkaConsumer {
+
+    @Autowired
+    private IncidentService incidentService;
+
+}
